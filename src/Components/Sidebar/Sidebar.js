63ea87ca20bc588cb813c62ae801b9b90904b1fa
Atul Sharma <theatsharma@gmail.com>
2018-05-29 00:20:19 +0530

    change folder name, update readme

---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+class Sidebar extends Component {
+	render() {
+		return (
+			<div className="App">
+				Sidebar
+			</div>
+		);
+	}
+}
+
+export default Sidebar;
--- src/Container/App/App.js
+++ src/Container/App/App.js
@@ -3,14 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import Button from '@material-ui/core/Button';
+import Sidebar from '../../Components/Sidebar/Sidebar';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Button variant="raised" color="primary">
-					Dashboard
-    			</Button>
+				<Sidebar />
 			</div>
 		);
 	}
